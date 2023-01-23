f1eb90c6b1b3030d1c2e9bca974ceb02a911d050
goodboyzuzu <gohzuhong@gmail.com>
2023-01-23 21:45:34 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,37 @@
+require("zuzu.plugins-setup")
+require("zuzu.core.options")
+require("zuzu.core.keymaps")
+require("zuzu.core.colorscheme")
+require("zuzu.plugins.comment")
+require("zuzu.plugins.nvim-tree")
+require("zuzu.plugins.lualine")
+require("zuzu.plugins.telescope")
+require("zuzu.plugins.nvim-cmp")
+require("zuzu.plugins.lsp.mason")
+require("zuzu.plugins.lsp.lspsaga")
+require("zuzu.plugins.lsp.lspconfig")
+require("zuzu.plugins.lsp.null-ls")
+require("zuzu.plugins.autopairs")
+require("zuzu.plugins.treesitter")
+require("zuzu.plugins.gitsigns")
+require("zuzu.plugins.barber")
+
+require("code_runner").setup({
+	-- put here the commands by filetype
+	filetype = {
+		java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+		python = "python3",
+		typescript = "deno run",
+		rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+		cpp = "cd $dir && g++ $fileName -Wall -Weffc++ -Wextra -Wsign-conversion -ggdb -std=c++20 -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
+		-- cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
+	},
+})
+
+vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>c", ":RunClose<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false })
